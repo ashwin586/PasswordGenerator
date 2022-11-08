da69c9a7bc9e2d454027eda47a771d97579ce558
Ashwin <ashwinv586@gmail.com>
2022-11-08 19:37:38 +0530

    First release of PasswordGenerator

---
+++ index.js
@@ -0,0 +1,30 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+                    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+                     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                     "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
+                    ];
+
+let psswdone = document.getElementById("psswd-1")
+let psswdtwo = document.getElementById("psswd-2")
+let length = 15;
+
+function generateChar() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar];
+}
+
+function generatePsswd () {
+   let randomPsswdOne = "";
+   for (let i=0; i<length; i++) {
+    randomPsswdOne += generateChar();
+   }
+   psswdone.textContent = randomPsswdOne;
+
+   let randomPsswdTwo = "";
+   for (let i=0; i<length; i++) {
+    randomPsswdTwo += generateChar();
+   }
+   psswdtwo.textContent = randomPsswdTwo;
+
+}
+
